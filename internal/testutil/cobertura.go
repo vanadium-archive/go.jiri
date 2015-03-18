41b8b25006481b86ae43814d64566f6a09427d15
Jing Jin <jingjin@google.com>
2015-03-17 19:12:49 -0700

    TBR: devtools/internal/testutil: fix path in cobertura.go
    Change-Id: I0e7fc3f0e34f65fde060076539b0c71eb200dcf2

--- internal/testutil/cobertura.go
+++ internal/testutil/cobertura.go
@@ -70,7 +70,7 @@ func coverageFromGoTestOutput(ctx *tool.Context, testOutput io.Reader) (*testCov
 	if err != nil {
 		return nil, err
 	}
-	bin := filepath.Join(root, "third_party", "go", "bin", "gocover-cobertura")
+	bin := util.ThirdPartyBinPath(root, "gocover-cobertura")
 	env, err := util.VanadiumEnvironment(ctx, util.HostPlatform())
 	if err != nil {
 		return nil, err
