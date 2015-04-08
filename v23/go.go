122ee17896eadb26f97c4697b8375a4f96f98d82
Todd Wang <toddw@google.com>
2015-04-07 18:18:59 -0700

    x/devtools: Move x/ref/lib/flags/buildinfo to x/lib/buildinfo
    This reduces package nesting, and is also nicer since the
    devtools/v23 tool uses this mechanism, and it's better for v23 to
    have absolutely no dependencies on x/ref/...
    
    MultiPart: 3/3
    Change-Id: Iccc240ca5c962929cc77afab73f706195b40d346

--- v23/go.go
+++ v23/go.go
@@ -113,7 +113,7 @@ func runXGo(command *cmdline.Command, args []string) error {
 }
 
 func setBuildInfoFlags(args []string, platform util.Platform) []string {
-	const buildInfoPackage = "v.io/x/ref/lib/flags/buildinfo"
+	const buildInfoPackage = "v.io/x/lib/buildinfo"
 
 	userName := ""
 	if currUser, err := user.Current(); err == nil {
