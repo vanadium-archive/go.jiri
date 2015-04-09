4d4f20361fca8dc3658cf38b5d0b56e3c71c3acb
Jing Jin <jingjin@google.com>
2015-04-09 14:53:08 -0700

    devtools/oncall: create data collector and server.
    These two things are kind of related so I put them in a single CL.
    
    "collector" talkes to
    - GCM to collect service latency data
    - GCE (using gsutil command) to collect GCE instance data (status for now)
    
    I decided to keep a history of data files so that we can support "going back in
    time" feature in the dashboard, which is cool. I've set a lifecycle config on
    the bucket so that all files older than 180 days are automatically deleted.
    
    "server" serves the latest data file as JSON. It also accepts a "ts" parameter
    to specify a specific data file (timestamp) to load.
    
    Some logic is similar to devtools/dashboard, but I think it still makes sense
    to separate them because this one will be used internally only.
    
    Change-Id: I5d9791e349fcc9f77767d718a91377b740741564

---
+++ internal/cache/cache.go
@@ -0,0 +1,45 @@
+// Copyright 2015 The Vanadium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package cache
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"v.io/x/devtools/internal/collect"
+	"v.io/x/devtools/internal/tool"
+)
+
+// StoreGoogleStorageFile reads the given file from the given Google Storage
+// location and stores it in the given cache location. It returns the cached
+// file path.
+func StoreGoogleStorageFile(ctx *tool.Context, cacheRoot, bucketRoot, filename string) (_ string, e error) {
+	cachedFile := filepath.Join(cacheRoot, filename)
+	if _, err := os.Stat(cachedFile); err != nil {
+		if !os.IsNotExist(err) {
+			return "", fmt.Errorf("Stat() failed: %v", err)
+		}
+		// To avoid interference between concurrent requests, download data to a
+		// tmp dir, and move it to the final location.
+		tmpDir, err := ctx.Run().TempDir(cacheRoot, "")
+		if err != nil {
+			return "", err
+		}
+		defer collect.Error(func() error { return ctx.Run().RemoveAll(tmpDir) }, &e)
+		if err := ctx.Run().Command("gsutil", "-m", "-q", "cp", "-r", bucketRoot+"/"+filename, tmpDir); err != nil {
+			return "", err
+		}
+		if err := ctx.Run().Rename(filepath.Join(tmpDir, filename), cachedFile); err != nil {
+			// If the target directory already exists, it must have been created by
+			// a concurrent request.
+			if !strings.Contains(err.Error(), "directory not empty") {
+				return "", err
+			}
+		}
+	}
+	return cachedFile, nil
+}
