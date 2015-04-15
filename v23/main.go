c69d960456b35a7f5d8aac7abd4e10529a0ee20b
Jiri Simsa <jsimsa@google.com>
2015-04-15 13:54:40 -0700

    devtools/v23: printing a warning when the first SIGINT is delivered to inform the user about the potential risks associated with interrupting the v23 tool. Second SIGINT terminates the v23 tool.
    Change-Id: Ic95a73490e941d77a5818e5d7c51c53998f0bb3b

--- v23/main.go
+++ v23/main.go
@@ -9,9 +9,90 @@
 package main
 
 import (
+	"fmt"
 	"os"
+	"os/exec"
+	"os/signal"
+	"syscall"
 )
 
+const warning = `
+############################################################################
+# WARNING: Forcefully terminating the operation of the v23 tool can leave  #
+# V23_ROOT in an inconsistent state. To terminate v23, press CTRL-C again. #
+############################################################################
+`
+
+func signalHandler(c chan os.Signal, p *os.Process) {
+	warned := false
+	for sig := range c {
+		switch sig {
+		case os.Interrupt:
+			if !warned {
+				fmt.Fprintf(os.Stdout, "%v", warning)
+				warned = true
+				continue
+			}
+		}
+		if err := p.Signal(sig); err != nil {
+			fmt.Fprintf(os.Stderr, "Signal(%v) failed: %v\n", sig, err)
+		}
+	}
+}
+
 func main() {
+	// If the operation of the v23 tool is forcefully terminated, it can
+	// leave the V23_ROOT directory in an inconsistent state. To lower
+	// the chance of users unintenionally corrupting their V23_ROOT, the
+	// v23 tool prints an informative warning message upon receiving the
+	// first SIGINT signal instead of terminating itself.
+	//
+	// Since the v23 tool spawns subprocesses such as git, simply
+	// catching the SIGINT signal in the v23 process is not sufficient
+	// because pressing CTRL-C in a shell delivers the SIGINT signal to
+	// all processes in the foreground process group.
+	//
+	// To assume control over which signals are delivered to the v23
+	// subprocesses, when the v23 tool starts, it spawns a copy of
+	// itself that runs under a different process group. The parent
+	// process handles incoming signals, while the child process
+	// executes the logic identified by the command-line arguments.
+	if os.Getenv("V23_CHILD") == "" {
+		os.Setenv("V23_CHILD", "1")
+		attr := &os.ProcAttr{
+			Files: []*os.File{os.Stdin, os.Stdout, os.Stderr},
+			Sys:   &syscall.SysProcAttr{Setpgid: true},
+		}
+		path, err := exec.LookPath(os.Args[0])
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "LookPath(%v) failed: %v\n", os.Args[0], err)
+			os.Exit(1)
+		}
+		p, err := os.StartProcess(path, os.Args, attr)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "StartProcess() failed: %v\n", err)
+			os.Exit(1)
+		}
+		// Create a channel for signal delivery. Its size determines the
+		// maximum number of signals that can be delivered concurrently
+		// before signals are dropped.
+		const maxConcurrentSignals = 10
+		c := make(chan os.Signal, maxConcurrentSignals)
+		signal.Notify(c)
+		go signalHandler(c, p)
+		state, err := p.Wait()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Wait() failed: %v\n", err)
+			os.Exit(1)
+		}
+		status, ok := state.Sys().(syscall.WaitStatus)
+		if !ok {
+			fmt.Fprintf(os.Stderr, "failed to retrieve the exit status\n")
+			os.Exit(1)
+		}
+		signal.Stop(c)
+		close(c)
+		os.Exit(status.ExitStatus())
+	}
 	os.Exit(root().Main())
 }
