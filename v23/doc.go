6775ac595ef7e2ee2d90000b0d87ed4812b83700
Todd Wang <toddw@google.com>
2015-04-22 16:53:48 -0700

    devtools: Add cmdline.HideGlobalFlagsExcept() to clean up help.
    Closes: veyron/release-issue#1905
    
    ---------- Example of new output ----------
    $ vrpc
    ERROR: vrpc: no command specified
    
    Command vrpc sends and receives Vanadium remote procedure calls.
    
    Usage:
    vrpc <command>
    
    The vrpc commands are:
    signature   Describe the interfaces of a Vanadium server
    call        Call a method of a Vanadium server
    identify    Reveal blessings presented by a Vanadium server
    help        Display help for commands or topics
    Run "vrpc help [command]" for command usage.
    
    Run "vrpc help -style=full" to show all global flags.
    ----------
    
    This CL shortens the default cmdline usage output, so that you
    have a chance at seeing the important parts before it scrolls off
    the screen.  The main culprit for long output is our long list of
    global flags.
    
    By default, nothing has changed; all global flags are shown in
    usage.  Calling cmdline.HideGlobalFlagsExcept() causes the
    default behavior to hide global flags, except for the given
    regexps.  This mechanism was chosen since it seems better to have
    global flags shown if the developer doesn't do anything special,
    but if they call the method it's easier to specify a whitelist
    than a blacklist.
    
    To show all global flags you use "help -style=full", or set the
    envvar CMDLINE_STYLE=full for root commands with no subcommands.
    When any global flags are hidden, the usage includes a line
    desribing the exact command to run to get the full output.
    
    The -style=godoc output always shows all global flags.
    
    MultiPart: 1/3
    
    Change-Id: Iaeadaa9cac405d15e6de6999fe4f27163c90f0f1

--- v23/doc.go
+++ v23/doc.go
@@ -30,7 +30,6 @@ The v23 commands are:
    xgo          Execute the go tool using the vanadium environment and
                 cross-compilation
    help         Display help for commands or topics
-Run "v23 help [command]" for command usage.
 
 The v23 flags are:
  -color=true
@@ -40,7 +39,7 @@ The v23 flags are:
  -v=false
    Print verbose output.
 
-V23 Api
+V23 api
 
 Use this command to ensure that no unintended changes are made to Vanadium's
 public API.
@@ -57,7 +56,7 @@ The v23 api flags are:
    The path to the gotools binary to use. If empty, gotools will be built if
    necessary.
 
-V23 Api Check
+V23 api check
 
 Check to see if any changes have been made to the public API.
 
@@ -67,7 +66,7 @@ Usage:
 <projects> is a list of Vanadium projects to check. If none are specified, all
 projects are checked.
 
-V23 Api Fix
+V23 api fix
 
 Updates the .api files to reflect your changes to the public API.
 
@@ -77,7 +76,7 @@ Usage:
 <projects> is a list of Vanadium projects to update. If none are specified, all
 project APIs are updated.
 
-V23 Buildcop
+V23 buildcop
 
 Manage vanadium build cop schedule. If no subcommand is given, it shows the LDAP
 of the current build cop.
@@ -89,14 +88,14 @@ Usage:
 The v23 buildcop commands are:
    list        List available build cop schedule
 
-V23 Buildcop List
+V23 buildcop list
 
 List available build cop schedule.
 
 Usage:
    v23 buildcop list
 
-V23 Cl
+V23 cl
 
 Manage vanadium changelists.
 
@@ -108,7 +107,7 @@ The v23 cl commands are:
    mail        Mail a changelist based on the current branch to Gerrit for
                review
 
-V23 Cl Cleanup
+V23 cl cleanup
 
 The cleanup command checks that the given branches have been merged into the
 master branch. If a branch differs from the master, it reports the difference
@@ -123,7 +122,7 @@ The v23 cl cleanup flags are:
  -f=false
    Ignore unmerged changes.
 
-V23 Cl Mail
+V23 cl mail
 
 Squashes all commits of a local branch into a single "changelist" and mails this
 changelist to Gerrit as a single commit. First time the command is invoked, it
@@ -157,7 +156,7 @@ The v23 cl mail flags are:
  -r=
    Comma-seperated list of emails or LDAPs to request review.
 
-V23 Contributors
+V23 contributors
 
 Lists vanadium project contributors and the number of their commits. Vanadium
 projects to consider can be specified as an argument. If no projects are
@@ -168,7 +167,7 @@ Usage:
 
 <projects> is a list of projects to consider.
 
-V23 Copyright
+V23 copyright
 
 This command can be used to check if all source code files of Vanadium projects
 contain the appropriate copyright header and also if all projects contains the
@@ -186,7 +185,7 @@ The v23 copyright flags are:
  -manifest=
    Name of the project manifest.
 
-V23 Copyright Check
+V23 copyright check
 
 Check copyright headers and licensing files.
 
@@ -195,7 +194,7 @@ Usage:
 
 <projects> is a list of projects to check.
 
-V23 Copyright Fix
+V23 copyright fix
 
 Fix copyright headers and licensing files.
 
@@ -204,7 +203,7 @@ Usage:
 
 <projects> is a list of projects to fix.
 
-V23 Env
+V23 env
 
 Print vanadium environment variables.
 
@@ -225,7 +224,7 @@ The v23 env flags are:
  -platform=
    Target platform.
 
-V23 Go
+V23 go
 
 Wrapper around the 'go' tool that can be used for compilation of vanadium Go
 sources. It takes care of vanadium-specific setup, such as setting up the Go
@@ -248,7 +247,7 @@ The v23 go flags are:
  -target-go=go
    Go command for the target platform.
 
-V23 Goext
+V23 goext
 
 Vanadium extension of the go tool.
 
@@ -258,7 +257,7 @@ Usage:
 The v23 goext commands are:
    distclean   Restore the vanadium Go workspaces to their pristine state
 
-V23 Goext Distclean
+V23 goext distclean
 
 Unlike the 'go clean' command, which only removes object files for packages in
 the source tree, the 'goext disclean' command removes all object files from
@@ -269,7 +268,7 @@ source tree.
 Usage:
    v23 goext distclean
 
-V23 Profile
+V23 profile
 
 To facilitate development across different platforms, vanadium defines
 platform-independent profiles that map different platforms to a set of libraries
@@ -282,14 +281,14 @@ The v23 profile commands are:
    list        List known vanadium profiles
    setup       Set up the given vanadium profiles
 
-V23 Profile List
+V23 profile list
 
 List known vanadium profiles.
 
 Usage:
    v23 profile list
 
-V23 Profile Setup
+V23 profile setup
 
 Set up the given vanadium profiles.
 
@@ -298,7 +297,7 @@ Usage:
 
 <profiles> is a list of profiles to set up.
 
-V23 Project
+V23 project
 
 Manage the vanadium projects.
 
@@ -311,7 +310,7 @@ The v23 project commands are:
    shell-prompt Print a succinct status of projects, suitable for shell prompts
    poll         Poll existing vanadium projects
 
-V23 Project Clean
+V23 project clean
 
 Restore vanadium projects back to their master branches and get rid of all the
 local branches and changes.
@@ -325,7 +324,7 @@ The v23 project clean flags are:
  -branches=false
    Delete all non-master branches.
 
-V23 Project List
+V23 project list
 
 Inspect the local filesystem and list the existing projects and branches.
 
@@ -339,7 +338,7 @@ The v23 project list flags are:
    If true, omit pristine projects, i.e. projects with a clean master branch and
    no other branches.
 
-V23 Project Shell-Prompt
+V23 project shell-prompt
 
 Reports current branches of vanadium projects (repositories) as well as an
 indication of each project's status:
@@ -357,7 +356,7 @@ The v23 project shell-prompt flags are:
  -show-name=false
    Show the name of the current repo.
 
-V23 Project Poll
+V23 project poll
 
 Poll vanadium projects that can affect the outcome of the given tests and report
 whether any new changes in these projects exist. If no tests are specified, all
@@ -372,7 +371,7 @@ The v23 project poll flags are:
  -manifest=
    Name of the project manifest.
 
-V23 Run
+V23 run
 
 Run an executable using the vanadium environment.
 
@@ -382,7 +381,7 @@ Usage:
 <executable> [arg ...] is the executable to run and any arguments to pass
 verbatim to the executable.
 
-V23 Snapshot
+V23 snapshot
 
 The "v23 snapshot" command can be used to manage snapshots of the vanadium
 project. In particular, it can be used to create new snapshots and to list
@@ -403,7 +402,7 @@ The v23 snapshot flags are:
  -remote=false
    Manage remote snapshots.
 
-V23 Snapshot Create
+V23 snapshot create
 
 The "v23 snapshot create <label>" command first checks whether the vanadium
 project configuration associates the given label with any tests. If so, the
@@ -445,7 +444,7 @@ The v23 snapshot create flags are:
  -time-format=2006-01-02T15:04:05Z07:00
    Time format for snapshot file name.
 
-V23 Snapshot List
+V23 snapshot list
 
 The "snapshot list" command lists existing snapshots of the labels specified as
 command-line arguments. If no arguments are provided, the command lists
@@ -456,7 +455,7 @@ Usage:
 
 <label ...> is a list of snapshot labels.
 
-V23 Test
+V23 test
 
 Manage vanadium tests.
 
@@ -469,7 +468,7 @@ The v23 test commands are:
    run         Run vanadium tests
    list        List vanadium tests
 
-V23 Test Generate
+V23 test generate
 
 The generate subcommand supports the vanadium integration test framework and
 unit tests by generating go files that contain supporting code. v23 test
@@ -524,7 +523,7 @@ The v23 test generate flags are:
  -progress=false
    Print verbose progress information.
 
-V23 Test Project
+V23 test project
 
 Runs tests for a vanadium project that is by the remote URL specified as the
 command-line argument. Projects hosted on googlesource.com, can be specified
@@ -536,7 +535,7 @@ Usage:
 
 <project> identifies the project for which to run tests.
 
-V23 Test Run
+V23 test run
 
 Run vanadium tests.
 
@@ -562,14 +561,14 @@ The v23 test run flags are:
  -v23.namespace.root=/ns.dev.v.io:8101
    The namespace root.
 
-V23 Test List
+V23 test list
 
 List vanadium tests.
 
 Usage:
    v23 test list
 
-V23 Update
+V23 update
 
 Updates all vanadium projects, builds the latest version of vanadium tools, and
 installs the resulting binaries into $V23_ROOT/devtools/bin. The sequence in
@@ -626,14 +625,14 @@ The v23 update flags are:
  -manifest=
    Name of the project manifest.
 
-V23 Version
+V23 version
 
 Print version of the v23 tool.
 
 Usage:
    v23 version
 
-V23 Xgo
+V23 xgo
 
 Wrapper around the 'go' tool that can be used for cross-compilation of vanadium
 Go sources. It takes care of vanadium-specific setup, such as setting up the Go
@@ -662,7 +661,7 @@ The v23 xgo flags are:
  -target-go=go
    Go command for the target platform.
 
-V23 Help
+V23 help
 
 Help with no args displays the usage of the parent command.
 
@@ -670,11 +669,10 @@ Help with args displays the usage of the specified sub-command or help topic.
 
 "help ..." recursively displays help for all commands and topics.
 
-The output is formatted to a target width in runes.  The target width is
-determined by checking the environment variable CMDLINE_WIDTH, falling back on
-the terminal width from the OS, falling back on 80 chars.  By setting
-CMDLINE_WIDTH=x, if x > 0 the width is x, if x < 0 the width is unlimited, and
-if x == 0 or is unset one of the fallbacks is used.
+Output is formatted to a target width in runes, determined by checking the
+CMDLINE_WIDTH environment variable, falling back on the terminal width, falling
+back on 80 chars.  By setting CMDLINE_WIDTH=x, if x > 0 the width is x, if x < 0
+the width is unlimited, and if x == 0 or is unset one of the fallbacks is used.
 
 Usage:
    v23 help [flags] [command/topic ...]
@@ -682,7 +680,11 @@ Usage:
 [command/topic ...] optionally identifies a specific sub-command or help topic.
 
 The v23 help flags are:
- -style=default
-   The formatting style for help output, either "default" or "godoc".
+ -style=compact
+   The formatting style for help output:
+      compact - Good for compact cmdline output.
+      full    - Good for cmdline output, shows all global flags.
+      godoc   - Good for godoc processing.
+   Override the default by setting the CMDLINE_STYLE environment variable.
 */
 package main
