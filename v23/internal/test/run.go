fd1fecf5f11c571d9a36ff9360c2103bcba5a714
Ross Wang <rosswang@google.com>
2015-07-21 12:52:54 -0700

    Adding Travel project tests to Jenkins.
    Change-Id: I1b2e63783e6858cef594caef5d1b94d442f1c061

--- v23/internal/test/run.go
+++ v23/internal/test/run.go
@@ -116,6 +116,7 @@ var testFunctions = map[string]func(*tool.Context, string, ...Opt) (*test.Result
 	"vanadium-release-candidate":          vanadiumReleaseCandidate,
 	"vanadium-release-candidate-snapshot": vanadiumReleaseCandidateSnapshot,
 	"vanadium-signup-proxy":               vanadiumSignupProxy,
+	"vanadium-travel-test":                vanadiumTravelTest,
 	"vanadium-www-config-deploy-prod":     vanadiumWWWConfigDeployProduction,
 	"vanadium-www-config-deploy-stag":     vanadiumWWWConfigDeployStaging,
 	"vanadium-www-deploy-production":      vanadiumWWWDeployProduction,
