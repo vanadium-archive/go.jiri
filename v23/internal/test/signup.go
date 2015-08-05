d29a230042a461d81dd7a7bd8c1a2af60b318b50
Jason Campbell <jasoncampbell@google.com>
2015-08-05 11:09:04 -0700

    devtools/signup: move sending welcome email logic into devtools
    MutliPart: 2/2
    Change-Id: Ib9994a9aa4b24aa72e71316504cf9f09dc74af11

--- v23/internal/test/signup.go
+++ v23/internal/test/signup.go
@@ -5,17 +5,86 @@
 package test
 
 import (
+	"bufio"
 	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
+	"v.io/x/devtools/internal/cache"
 	"v.io/x/devtools/internal/collect"
 	"v.io/x/devtools/internal/gitutil"
 	"v.io/x/devtools/internal/test"
 	"v.io/x/devtools/internal/tool"
 	"v.io/x/devtools/internal/util"
+
+	"gopkg.in/gomail.v1"
 )
+
+type link struct {
+	url  string
+	name string
+}
+
+type message struct {
+	lines []string
+	links map[string]link
+}
+
+func (m message) plain() string {
+	result := strings.Join(m.lines, "\n\n")
+
+	for key, link := range m.links {
+		result = strings.Replace(result, key, fmt.Sprintf("%v (%v)", link.name, link.url), -1)
+	}
+	return result
+}
+
+func (m message) html() string {
+	result := ""
+	for _, line := range m.lines {
+		result += fmt.Sprintf("<p>\n%v\n</p>\n", line)
+	}
+
+	for key, link := range m.links {
+		result = strings.Replace(result, key, fmt.Sprintf("<a href=%q>%v</a>", link.url, link.name), -1)
+	}
+	return result
+}
+
+var m = message{
+	lines: []string{
+		"Welcome to the Vanadium project. Your early access has been activated.",
+		"What now?",
+		"To understand a bit more about why we are building Vanadium and what we are trying to achieve with this early access program, read our [[0]].",
+		"The project’s website [[1]], includes information about Vanadium including key concepts, tutorials, and how to access our codebase.",
+		"Sign up for our mailing list, [[2]], and send any questions or feedback there.",
+		"As mentioned earlier, please keep this project confidential until it is publicly released. If there is anyone else that you think would benefit from access to this project, send them [[3]].",
+		"Thanks for participating,",
+		"The Vanadium Team",
+	},
+	links: map[string]link{
+		"[[0]]": link{
+			url:  "https://v.io/posts/001-welcome.html",
+			name: "welcome message",
+		},
+		"[[1]]": link{
+			url:  "https://v.io",
+			name: "v.io",
+		},
+		"[[2]]": link{
+			url:  "https://groups.google.com/a/v.io/forum/#!forum/vanadium-discuss",
+			name: "vanadium-discuss@v.io",
+		},
+		"[[3]]": link{
+			url:  "https://docs.google.com/a/google.com/forms/d/1IYq3fkmgqToqzVp0EAg3Oxv_7mtDzn6VCyMiiTdPNDY/viewform",
+			name: "here to sign up",
+		},
+	},
+}
 
 func vanadiumSignupProxy(ctx *tool.Context, testName string, _ ...Opt) (_ *test.Result, e error) {
 	return vanadiumSignupProxyHelper(ctx, "old_schema.go", testName)
@@ -88,14 +157,13 @@ func vanadiumSignupProxyHelper(ctx *tool.Context, schema, testName string) (_ *t
 	return &test.Result{Status: test.Passed}, nil
 }
 
-func vanadiumSignupWelcomeNew(ctx *tool.Context, testName string, _ ...Opt) (_ *test.Result, e error) {
+func vanadiumSignupWelcomeStepOneNew(ctx *tool.Context, testName string, _ ...Opt) (_ *test.Result, e error) {
 	root, err := util.V23Root()
 	if err != nil {
 		return nil, internalTestError{err, "VanadiumRoot"}
 	}
 
 	credentials := os.Getenv("CREDENTIALS")
-	emailCredentials := os.Getenv("EMAIL_CREDENTIALS")
 	sheetID := os.Getenv("SHEET_ID")
 
 	data, err := fetchFieldValues(ctx, credentials, "email", "new_schema.go", sheetID)
@@ -103,13 +171,40 @@ func vanadiumSignupWelcomeNew(ctx *tool.Context, testName string, _ ...Opt) (_ *
 		return nil, internalTestError{err, "fetch"}
 	}
 
+	var emails bytes.Buffer
+
 	welcome := filepath.Join(root, "infrastructure", "signup", "welcome.go")
 	welcomeOpts := ctx.Run().Opts()
 	welcomeOpts.Stdin = bytes.NewReader(data)
+	welcomeOpts.Stdout = &emails
 	sentlist := filepath.Join(root, "infrastructure", "signup", "sentlist.json")
-	nda := filepath.Join(root, "infrastructure", "signup", "google-agreement.pdf")
-	if err := ctx.Run().CommandWithOpts(welcomeOpts, "v23", "go", "run", welcome, "-credentials="+emailCredentials, "-sentlist="+sentlist, "-nda="+nda); err != nil {
+	if err := ctx.Run().CommandWithOpts(welcomeOpts, "v23", "go", "run", welcome, "-sentlist="+sentlist); err != nil {
 		return nil, internalTestError{err, "welcome"}
+	}
+
+	// Convert the newline delimited output from the command above into a slice of
+	// strings which can be written to a file in the format:
+	//
+	//   EMAILS = <email> <email> <email...>
+	//
+	output := []string{"EMAILS", "="}
+	reader := bytes.NewReader(emails.Bytes())
+	scanner := bufio.NewScanner(reader)
+	for scanner.Scan() {
+		email := scanner.Text()
+		output = append(output, email)
+	}
+
+	if err := scanner.Err(); err != nil {
+		return nil, internalTestError{err, "Scan"}
+	}
+
+	// Join the array and convert it to bytes
+	contents := strings.Join(output, " ")
+	filename := filepath.Join(root, ".vanadium_signup_weclome_properties")
+
+	if err := ctx.Run().WriteFile(filename, []byte(contents), 0644); err != nil {
+		return nil, internalTestError{err, "WriteFile"}
 	}
 
 	// Create a feature branch in the infrastructure project.
@@ -143,6 +238,47 @@ func vanadiumSignupWelcomeNew(ctx *tool.Context, testName string, _ ...Opt) (_ *
 		if err := ctx.Git(infraDir).Push("origin", "update:master", gitutil.VerifyOpt(false)); err != nil {
 			return nil, internalTestError{err, "push"}
 		}
+	}
+
+	return &test.Result{Status: test.Passed}, nil
+}
+
+func vanadiumSignupWelcomeStepTwoNew(ctx *tool.Context, testName string, _ ...Opt) (_ *test.Result, e error) {
+	root, err := util.V23Root()
+	if err != nil {
+		return nil, internalTestError{err, "VanadiumRoot"}
+	}
+
+	emailCredentials := os.Getenv("EMAIL_CREDENTIALS")
+	emails := strings.Split(os.Getenv("EMAILS"), " ")
+	bucket := os.Getenv("NDA_BUCKET")
+
+	// Download the NDA attachement from Google Cloud Storage
+	attachment, err := cache.StoreGoogleStorageFile(ctx, root, bucket, "google-agreement.pdf")
+	if err != nil {
+		return nil, internalTestError{err, "getNDA"}
+	}
+
+	// Use the Google Apps SMTP relay to send the welcome email, this has been
+	// pre-configured to allow authentiated v.io accounts to send mail.
+	credentials, err := readEmailCredentials(ctx, emailCredentials)
+	if err != nil {
+		return nil, internalTestError{err, "readEmailCredentials"}
+	}
+
+	mailer := gomail.NewMailer("smtp-relay.gmail.com", credentials.Email, credentials.Password, 587)
+	messages := []string{"sendWelcomeEmail errors:"}
+	for _, email := range emails {
+		if err := sendWelcomeEmail(mailer, email, attachment); err != nil {
+			messages = append(messages, err.Error())
+		}
+	}
+
+	// Log any errors from sending the email messages.
+	if len(messages) > 0 {
+		message := strings.Join(messages, "\n")
+		err := errors.New(message)
+		return nil, internalTestError{err, "sendWelcomeEmail"}
 	}
 
 	return &test.Result{Status: test.Passed}, nil
@@ -234,4 +370,46 @@ func fetchFieldValues(ctx *tool.Context, credentials, field, schema, sheetID str
 		return nil, err
 	}
 	return buffer.Bytes(), nil
+}
+
+func sendWelcomeEmail(mailer *gomail.Mailer, email string, attachment string) error {
+	message := gomail.NewMessage()
+	message.SetHeader("From", "Vanadium Team <welcome@v.io>")
+	message.SetHeader("To", email)
+	message.SetHeader("Subject", "Vanadium early access activated")
+	message.SetBody("text/plain", m.plain())
+	message.AddAlternative("text/html", m.html())
+
+	file, err := gomail.OpenFile(attachment)
+	if err != nil {
+		return fmt.Errorf("OpenFile(%v) failed: %v", attachment, err)
+	}
+
+	message.Attach(file)
+
+	if err := mailer.Send(message); err != nil {
+		return fmt.Errorf("Send(%v) failed: %v", message, err)
+	}
+
+	return nil
+}
+
+type emailCredentials struct {
+	Email    string `json:"email_username"`
+	Password string `json:"email_password"`
+}
+
+func readEmailCredentials(ctx *tool.Context, path string) (emailCredentials, error) {
+	var credentials emailCredentials
+
+	data, err := ctx.Run().ReadFile(path)
+	if err != nil {
+		return credentials, err
+	}
+
+	if err := json.Unmarshal(data, &credentials); err != nil {
+		return credentials, fmt.Errorf("Unmarshal(%v) failed: %v", data, err)
+	}
+
+	return credentials, nil
 }
