8dc0123ab4f3272a4395dd0dd904acc6d5ba7714
Cosmos Nicolaou <cnicolaou@google.com>
2015-09-19 18:21:42 -0700

    TBR: v.io/jiri/profiles: improve package comments.
    Change-Id: I399b242a13fbf0842c49af5f40689a3b696aea83

--- profiles/manager.go
+++ profiles/manager.go
@@ -2,20 +2,38 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-// Package profiles implements support for managing suites of software that
-// are required by particular applications. Such suites are called 'profiles'
-// and are managed by any package that implements the profiles.Manager interface.
-// Profiles essentially represent 'code' that is to be configured and compiled
-// for specified targets (profiles.Target). A single profile may be built
-// for multiple targets and in particular cross-compilation may be so implemented.
-// The common case is to build a profile for the target that represents the
-// same host that is built on (native compilation).
+// Package profiles implements support for managing external sofware dependencies.
+// It offers a balance between providing no support at all and a full blown package
+// manager. A profile is a named collection of software required for a given
+// system component or application. The name of the profile refers to all of the
+// required software, which may a single library or a collection of libraries or
+// SDKs. Profiles thus refer to uncompiled source code that needs to be compiled
+// for a specific "target". Targets represent compiled code and consist of:
+//
+// 1. A 'tag' that can be used a short hand for refering to a target
+// 2. An 'architecture' that refers to the CPU to be generate code for
+// 3. An 'operating system' that refers to the operating system to generate
+//    code for.
+// 4. An 'environment' which is a set of environment variables to use when
+//    compiling and using the profile.
+//
+// Targets provide the essential support for cross compilation.
 //
 // The profiles package provides a registry for profile implementations to
 // register themselves (by calling profiles.Register from an init function
 // for example) and for managing a 'manifest' of the currently built
 // profiles. The manifest is represented as an XML file.
-
+//
+// Profiles may be installed, updated or removed. When doing so, the name of
+// the profile is required, but the other components of the target are optional
+// and will default to the values of the system that the commands are run on
+// (so-called native builds). Once a profile is installed it may be referred to
+// by its tag for subsequent updates and removals. These operations are
+// defined by the profiles.Manager interface.
+//
+// The manifest tracks the installed profiles and their configurations.
+// Other command line tools and packages are expected read information about
+// the currently installed profiles from this manifest via profiles.ConfigHelper.
 package profiles
 
 import (
