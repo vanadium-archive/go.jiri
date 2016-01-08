cd37aa675a22c9cf63aac5f9179922944e131f29
Nicolas Lacasse <nlacasse@google.com>
2016-01-08 15:36:18 -0800

    jiri: Remove support for the -remote flag when creating snapshots.
    All the callers of "jiri snapshot -remote" have been transitioned to use
    --dir and --push-remote.
    
    I'll wait until next week before submitting this, in case I missed a
    caller.
    
    MultiPart: 1/2
    
    Change-Id: I7547f78c14fb8b62bfc815a687800aa10623af7d

--- doc.go
+++ doc.go
@@ -415,8 +415,6 @@ The jiri snapshot commands are:
 The jiri snapshot flags are:
  -dir=
    Directory where snapshot are stored.  Defaults to $JIRI_ROOT/.snapshot.
- -remote=false
-   Manage remote snapshots.
 
  -color=true
    Use color to format output.
@@ -469,8 +467,6 @@ The jiri snapshot create flags are:
    Directory where snapshot are stored.  Defaults to $JIRI_ROOT/.snapshot.
  -n=false
    Show what commands will run but do not execute them.
- -remote=false
-   Manage remote snapshots.
  -v=false
    Print verbose output.
 
@@ -492,8 +488,6 @@ The jiri snapshot list flags are:
    Directory where snapshot are stored.  Defaults to $JIRI_ROOT/.snapshot.
  -n=false
    Show what commands will run but do not execute them.
- -remote=false
-   Manage remote snapshots.
  -v=false
    Print verbose output.
 
